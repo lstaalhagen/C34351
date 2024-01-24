2b0979e71c2af97dd486e4f10fc291ce2ab08861
Lars Staalhagen <67311412+lstaalhagen@users.noreply.github.com>
2024-01-24 18:17:37 +0100

    Update install.sh

--- General/install.sh
+++ General/install.sh
@@ -3,6 +3,9 @@
 # Check for root
 [ $(id -u) -ne 0 ] && echo "Script must be executed with sudo" && exit 0
 
+apt-get update
+apt-get -y upgrade
+
 # Stop/disable some irrelevant services
 for p in avahi-daemon.service avahi-daemon.socket casper.service casper-md5check.service cups.service cups-browsed.service ModemManager.service ufw.service
 do
@@ -13,5 +16,3 @@ done
 # Add user to vboxsf group for shared folder
 usermod -aG vboxsf user
 
-# Misc general packages
-apt-get install -y git
