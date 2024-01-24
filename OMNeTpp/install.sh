b3bcdfc70615ae5b50d255894d2d16c744d280e9
Lars Staalhagen <67311412+lstaalhagen@users.noreply.github.com>
2024-01-24 18:48:12 +0100

    Update install.sh

--- OMNeTpp/install.sh
+++ OMNeTpp/install.sh
@@ -1,1 +1,10 @@
+#!/bin/sh
 
+# Check for root
+[ $(id -u) -ne 0 ] && echo "Script must be executed with sudo" && exit
+
+python -m pip install scipy numpy pandas matplotlib seaborn posix_ipc
+
+# Download and install OMNeT++
+cd /home/user
+curl https://github.com/omnetpp/omnetpp/releases/download/omnetpp-6.0.2/omnetpp-6.0.2-linux-x86_64.tgz
