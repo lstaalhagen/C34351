0b7c837d4c787f24f9e26459a632eaa05ad2d37c
Lars Staalhagen <67311412+lstaalhagen@users.noreply.github.com>
2024-01-25 14:02:47 +0100

    Update tst.sh

--- OMNeTpp/tst.sh
+++ OMNeTpp/tst.sh
@@ -5,13 +5,13 @@ CURRDIR=$(realpath .)
 cd /home/user/omnetpp-6.0.2
 
 USERNAME=$(who am i|cut -d ' ' -f 1)
-echo $USERNAME
+# echo $USERNAME
 
 HOMEDIR=$(eval echo "~$USERNAME")
-echo $HOMEDIR
+# echo $HOMEDIR
 
 rm -f $HOMEDIR/.local/share/applications/$OMNETVER-ide.desktop
 rm -f $HOMEDIR/.local/share/applications/$OMNETVER-shell.desktop
 
-echo ${CURRDIR}
-# install -m 0755 $(dirname $0)/omnetpp /usr/local/bin/omnetpp
+# echo ${CURRDIR}
+install -m 0755 ${CURRDIR}/omnetpp /usr/local/bin/omnetpp
