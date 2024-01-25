5c205ac2c50b1c8c7f69d6cb988eae729ce9f892
Lars Staalhagen <67311412+lstaalhagen@users.noreply.github.com>
2024-01-25 12:05:29 +0100

    Update install.sh

--- OMNeTpp/install.sh
+++ OMNeTpp/install.sh
@@ -4,11 +4,11 @@
 [ $(id -u) -ne 0 ] && echo "Script must be executed with sudo" && exit
 
 # Prerequisites
-apt-get install clang lld gdb bison flex perl swig qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools libqt5opengl5-dev libxml2-dev zlib1g-dev doxygen graphviz libwebkit2gtk-4.0-37
+# apt-get install clang lld gdb bison flex perl swig qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools libqt5opengl5-dev libxml2-dev zlib1g-dev doxygen graphviz libwebkit2gtk-4.0-37
 # python -m pip install scipy numpy pandas matplotlib seaborn posix_ipc
 
 # Get curl if not installed
-which curl &>/dev/null || apt-get -y install curl
+# which curl &>/dev/null || apt-get -y install curl
 
 # Download and install OMNeT++
 OMNETVER="omnetpp-6.0.2"
@@ -16,9 +16,13 @@ URL="https://github.com/omnetpp/omnetpp/releases/download/$OMNETVER/$OMNETVER-li
 TGZFILE=$(basename $URL)
 
 cd /opt
-curl -LG $URL -o $TGZFILE || exit
-tar xzf $TGZFILE
-rm -f $TGZFILE
+[ -d $OMNETVER ] && rm -rf $OMNETVER
+# curl -LG $URL -o $TGZFILE || exit
+tar -x -f $TGZFILE -z --no-same-owner
+# rm -f $TGZFILE
 cd $OMNETVER
 sed -i 's/WITH_OSG=yes/WITH_OSG=no/g' configure.user
-./source setenv && ./configure && make
+
+BASH=$(which bash)
+[ -z "$BASH" ] && echo "Bash must be installed" && exit
+$BASH -c "source setenv && ./configure && make"
