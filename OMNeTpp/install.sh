6689bbd1afbc204b48c4e4598b07b0d8926e8c8d
Lars Staalhagen <67311412+lstaalhagen@users.noreply.github.com>
2024-01-25 09:52:11 +0100

    Update install.sh

--- OMNeTpp/install.sh
+++ OMNeTpp/install.sh
@@ -5,7 +5,7 @@
 
 # Prerequisites
 apt-get install clang lld gdb bison flex perl swig qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools libqt5opengl5-dev libxml2-dev zlib1g-dev doxygen graphviz libwebkit2gtk-4.0-37
-python -m pip install scipy numpy pandas matplotlib seaborn posix_ipc
+# python -m pip install scipy numpy pandas matplotlib seaborn posix_ipc
 
 # Get curl if not installed
 which curl &>/dev/null || apt-get -y install curl
