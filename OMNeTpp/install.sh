2663e2c292ffc0c2dd718dc8cb68a0bbe7a4a2e1
Lars Staalhagen <67311412+lstaalhagen@users.noreply.github.com>
2024-01-25 14:05:59 +0100

    Update install.sh

--- OMNeTpp/install.sh
+++ OMNeTpp/install.sh
@@ -3,6 +3,9 @@
 # Check for root
 [ $(id -u) -ne 0 ] && echo "Script must be executed with sudo" && exit
 
+# Save current directory for later
+CURRDIR=$(realpath .)
+
 # Prerequisites
 # apt-get -y install clang lld gdb bison flex perl swig qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools libqt5opengl5-dev libxml2-dev zlib1g-dev doxygen graphviz libwebkit2gtk-4.0-37
 # python -m pip install scipy numpy pandas matplotlib seaborn posix_ipc
@@ -13,20 +16,20 @@ URL="https://github.com/omnetpp/omnetpp/releases/download/$OMNETVER/$OMNETVER-li
 TGZFILE=$(basename $URL)
 
 cd /home/user
-[ -d $OMNETVER ] && rm -rf $OMNETVER
-[ ! -f $TGZFILE ] && curl -LG $URL -o $TGZFILE || exit
-tar -x -f $TGZFILE -z
+# [ -d $OMNETVER ] && rm -rf $OMNETVER
+# [ ! -f $TGZFILE ] && curl -LG $URL -o $TGZFILE || exit
+# tar -x -f $TGZFILE -z
 # rm -f $TGZFILE
 
 cd $OMNETVER
-sed -i 's/WITH_OSG=yes/WITH_OSG=no/g' configure.user
+# sed -i 's/WITH_OSG=yes/WITH_OSG=no/g' configure.user
 
 BASH=$(which bash)
 [ -z "$BASH" ] && echo "Bash must be installed" && exit
-$BASH -c "source setenv && ./configure && make"
+# $BASH -c "source setenv && ./configure && make"
 
 USERNAME=$(who am i|cut -d ' ' -f 1)
 HOMEDIR=$(eval echo "~$USERNAME")
 rm -f $HOMEDIR/.local/share/applications/$OMNETVER-ide.desktop
 rm -f $HOMEDIR/.local/share/applications/$OMNETVER-shell.desktop
-install -m 0755 $(dirname $0)/omnetpp /usr/local/bin/omnetpp
+install -m 0755 ${CURRDIR}/omnetpp /usr/local/bin/omnetpp
