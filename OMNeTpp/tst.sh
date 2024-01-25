55ae5847906084f57c412f314ed78a5ec6099cc9
Lars Staalhagen <67311412+lstaalhagen@users.noreply.github.com>
2024-01-25 13:55:24 +0100

    Create tst.sh

---
+++ OMNeTpp/tst.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cd /home/user/omnetpp-6.0.2
+
+USERNAME=$(who am i|cut -d ' ' -f 1)
+echo $USERNAME
+
+HOMEDIR=$(eval echo "~$USERNAME")
+echo $HOMEDIR
+
+rm -f $HOMEDIR/.local/share/applications/$OMNETVER-ide.desktop
+rm -f $HOMEDIR/.local/share/applications/$OMNETVER-shell.desktop
+install -m 0755 $(dirname $0)/omnetpp /usr/local/bin/omnetpp
