3918ba81ca8a1dff35e3290b73c3049a7c85c0c9
Lars Staalhagen <lars@staalhagen.dk>
2024-02-02 14:39:34 +0100

    '20240202-1439'

---
+++ 01-General-install.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Check for root
+[ $(id -u) -ne 0 ] && echo "Script must be executed with sudo" && exit 0
+
+apt-get update
+apt-get -y upgrade
+apt-get -y autoremove
+
+# Stop/disable some irrelevant services
+for p in avahi-daemon.service avahi-daemon.socket cups.service cups-browsed.service ModemManager.service ufw.service apt-daily-upgrade.timer apt-daily.timer
+do
+   systemctl stop ${p}
+   systemctl mask ${p}
+done
+
+# Install misc
+apt-get -y install curl build-essential
+
+# Add user to vboxsf group for shared folder
+# usermod -aG vboxsf user
+
+# Cleanup apt-get
+apt-get clean
+
+# Fix Grub
+sed -i 's/GRUB_TIMEOUT_STYLE=.*/GRUB_TIMEOUT_STYLE=menu/g' /etc/default/grub
+sed -i 's/GRUB_TIMEOUT=.*/GRUB_TIMEOUT=2/g' /etc/default/grub
+update-grub
+
---
+++ 02-MininetWiFi-install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Check for root
+[ $(id -u) -ne 0 ] && echo "Script must be executed with sudo" && exit
+
+# Install Open vSwitch packages
+apt-get install -y openvswitch-common openvswitch-switch
+
+# Install Mininet-wifi
+git clone https://github.com/intrig-unicamp/mininet-wifi
+cd mininet-wifi
+util/install.sh -Wlnfv
+cd ..
+rm -rf mininet-wifi
+
+# Cleanup apt-get
+apt-get clean
