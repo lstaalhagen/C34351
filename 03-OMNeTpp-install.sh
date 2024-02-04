3303dff7ea5ccbdd3db82303bb344bbc922b990d
Lars Staalhagen <67311412+lstaalhagen@users.noreply.github.com>
2024-02-04 14:52:41 +0100

    Update 03-OMNeTpp-install.sh

--- 03-OMNeTpp-install.sh
+++ 03-OMNeTpp-install.sh
@@ -34,7 +34,7 @@ sed -i 's/PREFER_CLANG=yes/PREFER_CLANG=no/g' configure.user
 
 BASH=$(which bash)
 [ -z "$BASH" ] && echo "Bash must be installed" && exit
-sudo -u $USERNAME -R $HOMEDIR "$BASH -c \"cd $OMNETVER; source setenv && ./configure && make\""
+sudo -u $USERNAME "$BASH -c \"cd $HOMEDIR/$OMNETVER; source setenv && ./configure && make\""
 
 # Cleanup
 rm -f $HOMEDIR/.local/share/applications/$OMNETVER-ide.desktop
