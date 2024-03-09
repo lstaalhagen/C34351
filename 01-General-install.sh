d2227ecdd7103d1933726bb6e542a727d4ad57be
Lars Staalhagen <67311412+lstaalhagen@users.noreply.github.com>
2024-03-09 11:51:55 +0100

    Update 01-General-install.sh

--- 01-General-install.sh
+++ 01-General-install.sh
@@ -2,6 +2,8 @@
 
 # Check for root
 [ $(id -u) -ne 0 ] && echo "Script must be executed with sudo" && exit 0
+REALUSER=${SUDO_USER}
+[ -z "${REALUSER}" ] && echo "Environment variable $SUDO_USER not set as expected" && exit
 
 apt-get update
 apt-get -y upgrade
@@ -28,17 +30,17 @@ sed -i 's/GRUB_TIMEOUT_STYLE=.*/GRUB_TIMEOUT_STYLE=menu/g' /etc/default/grub
 sed -i 's/GRUB_TIMEOUT=.*/GRUB_TIMEOUT=2/g' /etc/default/grub
 update-grub
 
-# Set initial font for xterm
-XRESOURCES=/home/user/.Xresources
+# Hack to get a better default font size in Xterm windows
+XRESOURCES=/home/${REALUSER}/.Xresources
 if [ -s $XRESOURCES ]; then
   grep -q -e "^xterm*faceName:"
   if [ $? -ne 0 ]; then
     echo "xterm*faceName: Monospace" >> $XRESOURCES
     echo "xterm*faceSize: 14" >> $XRESOURCES
-    sudo -u user "xrdb -merge $XRESOURCES
+    sudo -u ${REALUSER} "xrdb -merge $XRESOURCES"
   fi
 else
   echo "xterm*faceName: Monospace" > $XRESOURCES
   echo "xterm*faceSize: 14" >> $XRESOURCES
-  sudo -u user "xrdb -merge $XRESOURCES
+  sudo -u ${REALUSER} "xrdb -merge $XRESOURCES"
 fi
