b70a906b81b75b7ade09ab61945a21485edd2b36
Lars Staalhagen <67311412+lstaalhagen@users.noreply.github.com>
2024-03-09 11:29:58 +0100

    Update 01-General-install.sh

--- 01-General-install.sh
+++ 01-General-install.sh
@@ -29,12 +29,16 @@ sed -i 's/GRUB_TIMEOUT=.*/GRUB_TIMEOUT=2/g' /etc/default/grub
 update-grub
 
 # Set initial font for xterm
-XRESOURCES=$HOME/.Xresources
-touch $XRESOURCES
-sed -i 's/^xterm*allowTitleOps:.*/xterm*allowTitleOps: false/g' $XRESOURCES
-grep -q -E -e "^xterm*faceName" $XRESOURCES
-if [ $? -ne 0 ]; then
-   echo "xterm*faceName: Monospace" > $HOME/.Xresources
-   echo "xterm*faceSize: 14" >> $HOME/.Xresources
-   xrdb -merge $HOME/.Xresources
+XRESOURCES=/home/user/.Xresources
+if [ -s $XRESOURCES ]; then
+  grep -q -e "^xterm*faceName:"
+  if [ $? -ne 0 ]; then
+    echo "xterm*faceName: Monospace" >> $XRESOURCES
+    echo "xterm*faceSize: 14" >> $XRESOURCES
+    sudo -u user "xrdb -merge $XRESOURCES
+  fi
+else
+  echo "xterm*faceName: Monospace" > $XRESOURCES
+  echo "xterm*faceSize: 14" >> $XRESOURCES
+  sudo -u user "xrdb -merge $XRESOURCES
 fi
