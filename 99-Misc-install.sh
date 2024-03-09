6065480fd41cb7e5c9b0778eb39ab62af59ff2b5
Lars Staalhagen <67311412+lstaalhagen@users.noreply.github.com>
2024-03-09 13:24:17 +0100

    Update 99-Misc-install.sh

--- 99-Misc-install.sh
+++ 99-Misc-install.sh
@@ -1,2 +1,16 @@
 #!/usr/bin/env bash
 
+# Some miscellaneous tweaks and fixes 
+
+# Check for root
+[ $(id -u) -ne 0 ] && echo "Script must be executed with sudo" && exit 0
+REALUSER=${SUDO_USER}
+[ -z "${REALUSER}" ] && echo "Environment variable $SUDO_USER not set as expected" && exit
+
+# Add (real) users to sudoers file
+grep -q -e "^${REALUSER}" /etc/sudoers
+if [ $? -ne 0]; then
+  echo -e "${REALUSER}\tALL = NOPASSWD: ALL" >> /etc/sudoers
+fi
+
+#
