dd843a946a74dcb2fd9e3cf7b1811135c3431d1e
Lars Staalhagen <67311412+lstaalhagen@users.noreply.github.com>
2024-03-09 13:06:27 +0100

    Update 04-MQTTCoAP.sh

--- 04-MQTTCoAP.sh
+++ 04-MQTTCoAP.sh
@@ -3,13 +3,34 @@
 # Check for root 
 [ $(id -u) -ne 0 ] && echo "Script must be executed with sudo" && exit
 
+# Install additional packages for both MQTT and CoAP
 apt-get update
 apt-get -y install mosquitto mosquitto-clients asciidoc 
+apt-get clean
 
+# Stop and mask the MQTT broker service, since we want to start it manually in the exercises
 systemctl stop mosquitto.service
 systemctl mask mosquitto.service
 
+# Download and install libcoap - takes some minutes
+git clone https://github.com/obgm/libcoap
+cd libcoap
+./autogen.sh
+./configure --enable-examples --enable-documentation
+make
+make install
+cd ..
+
+# Fix - set LD_LIBRARY_PATH environment variable in root's .bashrc so that it's set when coap-programs
+# are executed with sudo. Should perhaps have been set in user's .bashrc, but then sudo should be called
+# with the -E option.
+grep -q -e "^export LD_LIBRARY_PATH=" /root/.bashrc
+if [ $? -ne 0 ]; then
+  echo "export LD_LIBRARY_PATH=/usr/local/lib" >> /root/.bashrc
+fi
+
 # HACK GLOXBDITWPFR
+# Hack for setting PS1 to name of namespace if not default namespace
 grep -q -e "GLOXBDITWPFR" /root/.bashrc
 if [ $? -ne 0 ]; then
   echo '# HACK GLOXBDITWPFR' >> /root/.bashrc
